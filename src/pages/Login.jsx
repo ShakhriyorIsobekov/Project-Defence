3ff10b273ec17331d8fe2c1088244d7e382200a0
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2025-02-13 09:06:50 +0500

    add login page but not complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,126 @@
+// mui
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+// react hook form
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+// zod
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+});
+
 function Login() {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(data);
+    } catch (err) {
+      setError("email", {
+        message: "Error",
+      });
+    }
+  };
+
+  return (
+    <section className="login">
+      <Box
+        component={"div"}
+        sx={{
+          maxWidth: "900px",
+          width: "100%",
+          margin: "100px auto",
+          backgroundColor: "lightblue",
+          p: { xs: 3, sm: 5, md: 7, lg: 10 },
+          borderRadius: "20px",
+        }}
+      >
+        <Container maxWidth="lg">
+          <Box
+            sx={{ display: "flex", flexDirection: "column" }}
+            component={"form"}
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Typography
+              variant="h2"
+              sx={{
+                fontSize: {
+                  xs: "1.6rem",
+                  sm: "1.8rem",
+                  lg: "2rem",
+                },
+                fontWeight: "bold",
+                mb: 2,
+                textAlign: { xs: "center", sm: "left" },
+              }}
+            >
+              Login
+            </Typography>
+            <Typography
+              variant="h5"
+              sx={{
+                fontSize: { xs: "1rem", sm: "1rem", lg: "1.124rem" },
+                mb: 4,
+                textAlign: {
+                  xs: "center",
+                  sm: "left",
+                },
+              }}
+            >
+              Login in order to continue...
+            </Typography>
+            <TextField
+              required
+              id="outlined-required"
+              label="Email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <Typography
+                variant="h5"
+                sx={{ color: "red", mt: 1, fontSize: "1rem" }}
+              >
+                {errors.email.message}
+              </Typography>
+            )}
+            <TextField
+              required
+              id="outlined-required"
+              label="Password"
+              {...register("password")}
+              sx={{ mt: 6 }}
+            />
+            {errors.password && (
+              <Typography
+                variant="h5"
+                sx={{ color: "red", mt: 1, fontSize: "1rem" }}
+              >
+                {errors.password.message}
+              </Typography>
+            )}
+            <Button
+              sx={{ mt: 5 }}
+              disabled={isSubmitting}
+              type="submit"
+              variant="contained"
+            >
+              {isSubmitting ? "Loading..." : "Submit"}
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </section>
+  );
 }
 
 export default Login;
